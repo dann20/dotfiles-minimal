f0a9e4e94be86c3d3eb1493a214c9d7584f992e3
dann20 <nmdan0611@gmail.com>
2022-12-09 15:06:06 +0700

    Change surround plugin.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -386,7 +386,13 @@ lvim.plugins = {
     config = [[ require("configs.cmp_tabnine") ]],
   },
   {
-    "tpope/vim-surround", -- VimScript
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
   },
   {
     "phaazon/hop.nvim",
