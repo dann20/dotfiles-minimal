c5ba29ccd27f499330ffceeb47a6bfc838485908
dann20 <nmdan0611@gmail.com>
2023-06-11 14:48:29 +0700

    Fix integrating go tools to null ls

--- .config/lvim/lua/configs/go.lua
+++ .config/lvim/lua/configs/go.lua
@@ -1,9 +1,7 @@
 local gotest = require("go.null_ls").gotest()
 local gotest_codeaction = require("go.null_ls").gotest_action()
 local golangci_lint = require("go.null_ls").golangci_lint()
-local sources = {
-  gotest,
-  golangci_lint,
-  gotest_codeaction,
-}
-require("null-ls").setup { sources = sources, debounce = 1000, default_timeout = 5000 }
+
+require("null-ls").register(gotest)
+require("null-ls").register(gotest_codeaction)
+require("null-ls").register(golangci_lint)
